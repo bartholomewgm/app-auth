13a72b25db167eee38c29efff876c52debaf6c07
bartholomewgm <lutoshkin.bart@gmail.com>
2026-04-29 16:41:34 +0300

    Add CI with GitHub Actions and tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_login_page(client):
+    response = client.get(reverse('login'))
+    assert response.status_code == 200
